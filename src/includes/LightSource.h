bd628b78bd9aa9bde3d3efed9196002ad5d9ff1f
kwiatkowskim <kwiatkowski.michal.2003@gmail.com>
2025-01-30 23:52:57 +0100

    fading light

---
+++ src/includes/LightSource.h
@@ -0,0 +1,19 @@
+//
+// Created by michal on 1/30/25.
+//
+
+#ifndef LIGHTSOURCE_H
+#define LIGHTSOURCE_H
+
+#include <glm/glm.hpp>
+
+struct LightSource {
+    glm::vec3 position;
+    glm::vec3 color;
+
+    float constant;
+    float linear;
+    float quadratic;
+};
+
+#endif  // LIGHTSOURCE_H
--- src/shaders/lightning_fragment_shader.glsl
+++ src/shaders/lightning_fragment_shader.glsl
@@ -4,29 +4,53 @@ out vec4 FragColor;
 in vec3 Normal;
 in vec3 FragPos;
 
-uniform vec3 lightPos;
-uniform vec3 viewPos;
-uniform vec3 lightColor;
+struct Light {
+    vec3 position;
+    vec3 color;
+
+    float constant;
+    float linear;
+    float quadratic;
+};
+
+struct PhongProperties{
+    float ambientStrength;
+    float specularStrength;
+    float shininess;
+};
+
+uniform Light light;
+uniform PhongProperties phongProperties;
 uniform vec3 objectColor;
+uniform mat4 view;
 
 void main()
 {
+    // Transform light position to camera space
+    vec3 lightPosCamSpace = vec3(view * vec4(light.position, 1.0));
+
     // ambient
-    float ambientStrength = 0.1;
-    vec3 ambient = ambientStrength * lightColor;
+    vec3 ambient = phongProperties.ambientStrength * light.color;
 
     // diffuse
     vec3 norm = normalize(Normal);
-    vec3 lightDir = normalize(lightPos - FragPos);
+    vec3 lightDir = normalize(lightPosCamSpace - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = diff * lightColor;
+    vec3 diffuse = diff * light.color;
 
     // specular
-    float specularStrength = 0.5;
-    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 viewDir = normalize(-FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
-    vec3 specular = specularStrength * spec * lightColor;
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), phongProperties.shininess);
+    vec3 specular = phongProperties.specularStrength * spec * light.color;
+
+    // attenuation
+    float distance    = length(lightPosCamSpace - FragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
+
+    ambient  *= attenuation;
+    diffuse  *= attenuation;
+    specular *= attenuation;
 
     vec3 result = (ambient + diffuse + specular) * objectColor;
     FragColor = vec4(result, 1.0);
