d24c9276dadd2da3ec2534b19949cf151e5232d1
kwiatkowskim <kwiatkowski.michal.2003@gmail.com>
2025-02-09 16:21:04 +0100

    basic lightning. specular yet to fix

--- src/shaders/lightning_pass_fragment_shader.glsl
+++ src/shaders/lightning_pass_fragment_shader.glsl
@@ -16,22 +16,43 @@ struct Light {
     float quadratic;
 };
 
+struct PhongProperties{
+    float ambientStrength;
+    float specularStrength;
+    float shininess;
+};
+
+uniform Light light;
+uniform PhongProperties phongProperties;
 uniform vec3 view;
 
 void main()
 {
+    vec3 lightPosCamSpace = light.position;
+
     // retrieve data from gbuffer
     vec3 FragPos = texture(gPosition, TexCoords).rgb;
     vec3 Normal = texture(gNormal, TexCoords).rgb;
     vec3 Diffuse = texture(gAlbedoSpec, TexCoords).rgb;
     float Specular = texture(gAlbedoSpec, TexCoords).a;
 
-    // then calculate lighting as usual
-    vec3 lighting  = Diffuse * 0.1; // hard-coded ambient component
-    vec3 viewDir  = normalize(view - FragPos);
+    // Ambient component
+    vec3 ambient = phongProperties.ambientStrength * light.color * Diffuse;
 
-    // Display only specular for debugging purposes
-    vec3 result = vec3(Specular, Specular, Specular);
-    FragColor = vec4(result, 1.0);
+    // Diffuse component
+    vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(lightPosCamSpace - FragPos);
+    float diff = max(dot(norm, lightDir), 0.0);
+    vec3 diffuse = diff * light.color * Diffuse;
+
+    // specular
+    vec3 viewDir = normalize(view - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), phongProperties.shininess);
+    vec3 specular = spec * light.color *  Specular;
+
+
+    vec3 lightning = ambient + diffuse + specular;
+    FragColor = vec4(specular, 1.0);
 }
 
